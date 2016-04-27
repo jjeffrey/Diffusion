2af4f72892ad4fa3a508df61048b9cd12df019a0
local admin <local admin>
2016-04-27 16:36:42 -0400

    Update urls

--- djproject/diffusionCenter/diffusion/views.py
+++ djproject/diffusionCenter/diffusion/views.py
@@ -5,3 +5,17 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Hello, world, you're at the diffusion center!")
+
+def glossarySearch(request, query):
+		message = 'You submitted: ' + query
+		return HttpResponse(message)
+
+def editGlossary(request):
+	return HttpResponse("You're editing the glossary with a POST request, right?");
+# def setTrial(request, query="diffusion"):
+
+def integrate (request, terms):
+	tArray = terms.split('/')
+	tupArray = map(lambda x: tuple(x.split('-')), tArray)
+	print(tupArray[0])
+	return HttpResponse(", ".join(tArray))
