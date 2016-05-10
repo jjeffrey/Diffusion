01457d7c57c64357e4d749382059b7cd98523abf
Jonathan Jeffrey <jonrjeffrey@gmail.com>
2016-05-09 22:09:14 -0400

    Add models and reference format for JSON

--- djproject/diffusionCenter/diffusion/models.py
+++ djproject/diffusionCenter/diffusion/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class ContactProfile(models.Model)
+	length_nm = models.IntegerField(default=50)
+	profile = models.TextField()
+
+class Treatment(models.Model)
+	RTA_TYPES = (
+        ('F', 'Furnace'),
+        ('L', 'Lamp'),
+    )
+    rta_type = models.CharField(max_length=1, choices=RTA_TYPES)
+    treatment_times = models.TextField()
+
+#decribes material in isolation
+class Material(models.Model)
+	name = models.CharField(max_length=50)
+	short_name = models.CharField(max_length=10)
+	heat_transfer_coefficient = models.FloatField()
+	melting_point = models.FloatField()
+	additional_data = models.TextField() #A JSON (its fields may need to be later added to model)
+
+#describes how different materials diffuse through one another
+class DiffusionRelation(models.Model)
+	DIFFUSION_TYPES = (
+        ('INTER', 'Interdiffusion'),
+        ('IMPUR', 'Impurity'),
+        ('SELF', 'Self'),
+    )
+	diffusion_type models.CharField(max_length=5, choices=DIFFUSION_TYPES)
+	material_1 = models.CharField(max_length=10) #short_name of material in higher concentration
+	material_2 = models.CharField(max_length=10) #short_name of material in lower concentration
+	additional_data = models.TextField() #A JSON (its fields may need to be later added to model)
+
+
