7d1e8a37f7e9f5e2744465829de62616d7f51c75
Jonathan Jeffrey <jonrjeffrey@gmail.com>
2016-05-11 08:56:06 -0400

    Include app and update models

--- djproject/diffusionCenter/diffusion/models.py
+++ djproject/diffusionCenter/diffusion/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class ContactProfile(models.Model)
-	length_nm = models.IntegerField(default=50)
-	profile = models.TextField()
+class ContactProfile(models.Model):
+    length_nm = models.IntegerField(default=50)
+    profile = models.TextField()
 
-class Treatment(models.Model)
-	RTA_TYPES = (
+class Treatment(models.Model):
+    name = models.CharField(max_length=100)
+    RTA_TYPES = (
         ('F', 'Furnace'),
         ('L', 'Lamp'),
     )
@@ -14,23 +15,24 @@ class Treatment(models.Model)
     treatment_times = models.TextField()
 
 #decribes material in isolation
-class Material(models.Model)
-	name = models.CharField(max_length=50)
-	short_name = models.CharField(max_length=10)
-	heat_transfer_coefficient = models.FloatField()
-	melting_point = models.FloatField()
-	additional_data = models.TextField() #A JSON (its fields may need to be later added to model)
+class Material(models.Model):
+    name = models.CharField(max_length=50)
+    short_name = models.CharField(max_length=10)
+    heat_transfer_coefficient = models.FloatField()
+    thermal_conductivity = models.FloatField()
+    melting_point = models.FloatField()
+    additional_data = models.TextField() #A JSON (its fields may need to be later added to model)
 
 #describes how different materials diffuse through one another
-class DiffusionRelation(models.Model)
-	DIFFUSION_TYPES = (
+class DiffusionRelation(models.Model):
+    DIFFUSION_TYPES = (
         ('INTER', 'Interdiffusion'),
         ('IMPUR', 'Impurity'),
         ('SELF', 'Self'),
     )
-	diffusion_type models.CharField(max_length=5, choices=DIFFUSION_TYPES)
-	material_1 = models.CharField(max_length=10) #short_name of material in higher concentration
-	material_2 = models.CharField(max_length=10) #short_name of material in lower concentration
-	additional_data = models.TextField() #A JSON (its fields may need to be later added to model)
+    diffusion_type = models.CharField(max_length=5, choices=DIFFUSION_TYPES)
+    material_1 = models.CharField(max_length=10) #short_name of material in higher concentration
+    material_2 = models.CharField(max_length=10) #short_name of material in lower concentration
+    additional_data = models.TextField() #A JSON (its fields may need to be later added to model)
 
 
--- djproject/diffusionCenter/diffusionCenter/settings.py
+++ djproject/diffusionCenter/diffusionCenter/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'diffusion.apps.DiffusionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
