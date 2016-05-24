753a1da16ca08b611e0dc4aa92914bd338006b44
Jonathan Jeffrey <jonrjeffrey@gmail.com>
2016-05-24 09:56:23 -0400

    General site format

--- djproject/diffusionCenter/diffusion/urls.py
+++ djproject/diffusionCenter/diffusion/urls.py
@@ -2,8 +2,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^glossary/(?P<query>\w+)/', views.glossarySearch),
-	url(r'^edit/glossary/', views.editGlossary),
-	url(r'^integrate(?P<terms>(/[0-9]-[0-9])+)', views.integrate),
+	url(r'^glossary/?$', views.glossary_search),
+	url(r'^glossary/(?P<query>\w+)/?$', views.glossary_search),
+	url(r'^edit/glossary/?$', views.editGlossary),
+	#url(r'^integrate(?P<terms>(/[0-9]-[0-9])+)/?$', views.integrate),
+	url(r'^setup-profile/?$', views.setup_profile),
+	url(r'^setup-process/?$', views.setup_process),
+	url(r'^run-process/?$', views.run_process),
+	url(r'^saved-results/?$', views.saved_results),
 	url(r'^', views.index, name='index')
 ]
--- djproject/diffusionCenter/diffusion/views.py
+++ djproject/diffusionCenter/diffusion/views.py
@@ -5,17 +5,9 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-	material_list = Material.objects.order_by("short_name")
-	short_name_list = [material.short_name for material in material_list]
-	context = {
-		'short_name_list': short_name_list,
-	} 
-	print(material_list)
-	template = loader.get_template('main.html')
-	return HttpResponse(template.render(context, request))
+	return render(request,'main.html')
 
-def glossarySearch(request, query):
-	
+def glossary_search(request, query=''):
 	context = {
 		'material_list': Material.objects.order_by("short_name"),
 		#'relation_list': DiffusionRelation.order_by('diffusion_type').order_by('material_1').order_by('material_2')
@@ -25,11 +17,35 @@ def glossarySearch(request, query):
 
 def editGlossary(request):
 	return HttpResponse("You're editing the glossary with a POST request, right?")
-# def setTrial(request, query="diffusion"):
 
-def integrate (request, terms):
-	tArray = terms.split('/')[1:]
-	tupArray = list(map(lambda x: tuple(x.split('-')), tArray))
-	t2Array = list(map(lambda x: x[0]+"x^"+x[1], tupArray))
-	print(tupArray)
-	return HttpResponse("<b>Input Data:</b>  " + ", ".join(tArray) + "<br><b>Parsed Polynomial:</b> " + " + ".join(t2Array))
+# def integrate (request, terms):
+# 	tArray = terms.split('/')[1:]
+# 	tupArray = list(map(lambda x: tuple(x.split('-')), tArray))
+# 	t2Array = list(map(lambda x: x[0]+"x^"+x[1], tupArray))
+# 	print(tupArray)
+# 	return HttpResponse("<b>Input Data:</b>  " + ", ".join(tArray) + "<br><b>Parsed Polynomial:</b> " + " + ".join(t2Array))
+
+def setup_profile(request):
+	return render(request, 'profile_setup.html')
+
+def setup_process(request):
+	return render(request, 'process_setup.html')
+
+def run_process(request):
+	return render(request, 'process_run.html')
+
+def saved_results(request):
+	return render(request, 'saved_results.html')
+
+def add_material (request):
+	#if request.method == 'POST'
+	return HttpResponse("hi")
+
+def add_process(request):
+	return HttpResponse("hi")
+
+def add_profile(request):
+	return HttpResponse("hi")
+
+def get_result(request):
+	return HttpResponse("hi")
